9f74555de7b82a30b282a6790e997d8d94df9d3d
daniel.vadas@promera.hu <daniel.vadas@promera.hu>
2018-12-07 18:46:50 +0100

    basic frontend

--- Szofttech/src/back/Backend.java
+++ Szofttech/src/back/Backend.java
@@ -1,4 +1,6 @@
 package src.back;
 
 public class Backend {
+
+    public void listAds(){}
 }
--- Szofttech/src/front/Frontend.java
+++ Szofttech/src/front/Frontend.java
@@ -3,10 +3,10 @@ package src.front;
 public class Frontend {
 
     public void start(){
-        System.out.println("Üdvözlünk a Munkaközvetítő cégnél." +
+        System.out.println("Üdvözlünk a Munkaközvetítő cégnél.\n" +
                 "Kérlek válassz az alábbi listából:\n" +
-                "(1) Log In\n" +
-                "(2) Registration\n" +
-                "(3) Login as guest");
-    }
+                "(1) Belépés\n" +
+                "(2) Regisztráció\n" +
+                "(3) Belépés vendégként");
+        }
 }
