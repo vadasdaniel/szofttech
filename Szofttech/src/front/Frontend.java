a6db69422e4115b0ba487b7a61c9baad3c8f7be8
daniel.vadas@promera.hu <daniel.vadas@promera.hu>
2018-12-07 18:38:20 +0100

    basic frontend v1

--- Szofttech/src/front/Frontend.java
+++ Szofttech/src/front/Frontend.java
@@ -1,4 +1,12 @@
 package src.front;
 
 public class Frontend {
+
+    public void start(){
+        System.out.println("Üdvözlünk a Munkaközvetítő cégnél." +
+                "Kérlek válassz az alábbi listából:\n" +
+                "(1) Log In\n" +
+                "(2) Registration\n" +
+                "(3) Login as guest");
+    }
 }
