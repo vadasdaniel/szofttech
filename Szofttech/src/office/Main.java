820f66f90b388b20db44aa52c53cfefc0ef77275
Szendi-Horváth József <szendi-horvath.jozsef@promera.hu>
2018-12-09 17:19:39 +0100

    Fix to main class

--- Szofttech/src/office/Main.java
+++ Szofttech/src/office/Main.java
@@ -1,41 +1,19 @@
 package office;
 
+import back.Backend;
 import front.Frontend;
-import storage.company.CompanyManager;
-import storage.handledSubscribtions.HandledSubscriptionsManager;
-import storage.jobAd.JobAdManager;
-import storage.subscribe.SubscribeManager;
-import storage.user.UserManager;
-
-import java.io.IOException;
 
 
 public class Main {
 
-    private JobAdManager jobAdManager;
-    private CompanyManager companyManager;
-    private HandledSubscriptionsManager handledSubscriptionsManager;
-    private SubscribeManager subscribeManager;
-    private UserManager userManager;
-
     public static void main(String[] args) {
-        Frontend frontend;
-        frontend = new Frontend();
-
-        frontend.start();
+        new Main().start();
     }
 
-    public Main(
-            JobAdManager jobAdManager,
-            CompanyManager companyManager,
-            HandledSubscriptionsManager handledSubscriptionsManager,
-            SubscribeManager subscribeManager,
-            UserManager userManager
-    ) {
-        this.jobAdManager = jobAdManager;
-        this.companyManager = companyManager;
-        this.handledSubscriptionsManager = handledSubscriptionsManager;
-        this.subscribeManager = subscribeManager;
-        this.userManager = userManager;
+    private void start() {
+        Backend backend = new Backend();
+        Frontend frontend = new Frontend(backend);
+
+        frontend.start();
     }
 }
